77b31a84f23bb498ccae674b330174e2b3b24046
Ezequiel Gonzalez <ezeqiel111@gmail.com>
2023-02-11 00:45:38 -0300

    agregue ejercicio b y c

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 //a) Pida al usuario su nombre y muestre un mensaje de bienvenida.
-prompt ("ingrese su nombre")
- alert("bienvenido!")
+//prompt ("ingrese su nombre")
+// alert("bienvenido!")
+//b) Pida al usuario dos números y muestre su suma.
+
+/*let num1 = prompt ("ingrese numero")
+let num2 = prompt ("ingrese numero")
+
+console.log (parseInt(num1) + parseInt(num2))*/
+
+//c) Pida al usuario dos números y muestre el mayor.
+
+let num3 = prompt ("ingrese numero")
+let num4 = prompt ("ingrese numero")
+
+if (parseInt(num3) > parseInt(num4)){
+console.log(`num3 $"es mayor"`)
+}else console.log (num4)
+
