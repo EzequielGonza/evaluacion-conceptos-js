1356cf149393053f56bc2af149b9edaa4fc18b36
Ezequiel Gonzalez <ezeqiel111@gmail.com>
2023-02-11 02:01:13 -0300

    agregué ejercicio e

--- app.js
+++ app.js
@@ -19,10 +19,31 @@ console.log(num3)
 
 //d)Pida al usuario tres números y muestre una leyenda que diga si los tres son iguales, sino lo son que también lo informe.
 
-let num5 = prompt ("ingrese numero");
+/*let num5 = prompt ("ingrese numero");
 let num6 = prompt ("ingrese numero");
 let num7 = prompt ("ingrese numero");
 
 if (num5 == num6 == num7){
 console.log ("los numeros son iguales")
-}else console.log("los numeros no son igualesS")
+}else {console.log("los numeros no son iguales")
+}*/
+//e) Pida al usuario tres números y muéstrelos ordenados de mayor a menor.
+
+let num8 = parseint (prompt ("ingrese el primer numero"));
+let num9 = parseint (prompt ("ingrese el segundo numero"));
+let num10 = parseint (prompt ("ingrese el tercer numero"));
+
+if (num8 >= num9 >= num10){
+    console.log(num8 + num9 + num10)
+}else if (num8 >= num10 >= num9)
+    console.log(num8 + num10 + num9)
+ else if (num9 >= num8 >= num10)
+    console.log(num9 + num8 + num10)
+ else if (num9 >= num10 >= num8)
+    console.log(num9 + num10 + num8)
+ else if (num10 >= num9 >= num8)
+    console.log(num9 + num9 + num8)
+    else {console.log(num10 + num8 + num9)
+}
+
+
