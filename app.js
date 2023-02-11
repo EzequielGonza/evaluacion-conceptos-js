1f72ecd91f20110e2b163717b666446e0065f3b1
Ezequiel Gonzalez <ezeqiel111@gmail.com>
2023-02-11 01:02:16 -0300

    agregué ejercicio d

--- app.js
+++ app.js
@@ -10,10 +10,19 @@ console.log (parseInt(num1) + parseInt(num2))*/
 
 //c) Pida al usuario dos números y muestre el mayor.
 
-let num3 = prompt ("ingrese numero")
+/*let num3 = prompt ("ingrese numero")
 let num4 = prompt ("ingrese numero")
 
 if (parseInt(num3) > parseInt(num4)){
-console.log(`num3 $"es mayor"`)
-}else console.log (num4)
+console.log(num3)
+}else console.log (num4)*/
 
+//d)Pida al usuario tres números y muestre una leyenda que diga si los tres son iguales, sino lo son que también lo informe.
+
+let num5 = prompt ("ingrese numero");
+let num6 = prompt ("ingrese numero");
+let num7 = prompt ("ingrese numero");
+
+if (num5 == num6 == num7){
+console.log ("los numeros son iguales")
+}else console.log("los numeros no son igualesS")
